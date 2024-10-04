dad2dcc967179cc0699262687cdad5697c49c640
Julio Linarez <julio.lanzerz@gmail.com>
2024-10-04 16:19:43 -0400

    primera entrada con comentarios

--- app/route/test_route.php
+++ app/route/test_route.php
@@ -3,14 +3,20 @@ use App\Controller\TestController;
 use App\Config\Middleware;
 
 
-$app->group('/test/',function(){
+// Grupo de rutas bajo el prefijo /test/
+$app->group('/test/', function() {
+    // Definición de middleware para proteger la ruta test_middleware
+    $middleware = Middleware::class . ":onlyUser ";
 
-    $middleware = Middleware::class.":onlyUser";;
+    // Ruta GET /test que llama al método test de la clase TestController
+    $this->get('test', TestController::class . ":test");
 
-    $this->get('test', TestController::class.":test");
-    $this->post('test_middleware', TestController::class.":test")->add($middleware);
-    $this->get('get', TestController::class.":get");
+    // Ruta POST /test_middleware que llama al método test de la clase TestController
+    // y utiliza el middleware onlyUser  para proteger la ruta
+    $this->post('test_middleware', TestController::class . ":test")->add($middleware);
 
+    // Ruta GET /get que llama al método get de la clase TestController
+    $this->get('get', TestController::class . ":get");
 });
 
 
