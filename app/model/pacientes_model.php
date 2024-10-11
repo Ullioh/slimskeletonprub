1aa8d67450ff754e7284b87eca348ac9e7f44f70
Julio Linarez <julio.lanzerz@gmail.com>
2024-10-11 16:17:52 -0400

    cambio

---
+++ app/model/pacientes_model.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Model;
+use App\Lib\Database;
+use App\Config\ConfigGlobal;
+use App\Config\InitModel;
+
+class ArticulosModel extends InitModel{
+    private $db;
+    private $table = 'articulos';
+    private $column = array("nombre","descripcion","cantidad");
+    private $column_select = array("id","nombre","descripcion","cantidad");
+    private $column_update = array("descripcion","cantidad");
+    private $column_filter = array("nombre","descripcion","cantidad");
+    private $url;
+
+    public function __CONSTRUCT()
+    {
+        $CONFIG_G = new ConfigGlobal();
+        $this->url = $CONFIG_G->getServer();
+        parent::__construct(array(
+            "table"             =>  $this->table,
+            "column"            =>  $this->column,
+            "column_select"     =>  $this->column_select,
+            "column_update"     =>  $this->column_update,
+            "column_filter"     =>  $this->column_filter,
+            "column_status"     =>  array(
+                "name"      =>  "status",
+                "deleted"   =>  0
+            )
+        ));
+        $this->db = Database::StartUp();
+    }
+
+    public function personalizado($data){
+        try {
+            $sql = "SELECT * FROM $this->table WHERE id = ".$data;  
+            $stm = $this->db->prepare($sql);
+            $stm->execute();
+
+            return $stm->fetch();
+        } catch(Exception $e){
+            return $e;
+        }
+    }
+
+    // public function test($data){
+    //     try {
+    //         $sql = "SELECT * FROM $this->table WHERE id = ".$data['id'];  
+    //         $stm = $this->db->prepare($sql);
+    //         $stm->execute();
+
+    //         return $stm->fetch();
+    //     } catch(Exception $e){
+    //         return $e;
+    //     }
+    // }
+
+    // public function testInsert($data){
+    //     try{
+    //         $sql= "INSERT INTO $this->table(email, clave, nombre, cedula, ocupacion, empresa, rif, direccion, id_estado, id_subzona, ciudad, telefono, celular, fecha_nacimiento, status) 
+    //         VALUE(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    //         $this->db->prepare($sql)->execute(array(
+    //             $data['email'],
+    //             $data['clave'],
+    //             $data['nombre'],
+    //             $data['cedula'],
+    //             $data['ocupacion'],
+    //             $data['empresa'],
+    //             $data['rif'],
+    //             $data['direccion'],
+    //             $data['id_estado'],
+    //             $data['id_subzona'],
+    //             $data['ciudad'],
+    //             $data['telefono'],
+    //             $data['celular'],
+    //             $data['fecha_nacimiento'],
+    //             1,
+    //         ));
+    //         $id = $this->db->lastInsertId();
+
+    //         $data = $id;
+
+    //         return $data;
+    //     }catch(Exception $e){
+    //         return $e;
+    //     }
+    // }
+
+}
+?>
